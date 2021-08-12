1bd9b9c64011908bf4dd733a20d8f3b05e4a4445
Jesse Nauman <jnauman619@gmail.com>
2021-08-11 22:03:02 -0500

    Commented out correct code

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-const getData = async () => {
-    let season = document.querySelector('#season').value
-    let round = document.querySelector('#round').value
-    let response = await axios.get(`http://ergast.com/api/f1/${season}/${round}/driverStandings.json`)
-    return response.data
-}
+// const getData = async () => {
+//     let season = document.querySelector('#season').value
+//     let round = document.querySelector('#round').value
+//     let response = await axios.get(`http://ergast.com/api/f1/${season}/${round}/driverStandings.json`)
+//     return response.data
+// }
 
 // const submit = () => {
 //     document.getElementById("table-container").innerHTML = '';
